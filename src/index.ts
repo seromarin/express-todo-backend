831a924dd30707de9fef84d7e797b42f00b01bb1
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-05-17 15:47:30 -0500

    Add compression

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import express, { Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
+import compression from 'compression';
 
-dotenv.config();
-
-const app: Application = express();
-
+// Global Variables
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req: Request, res: Response) => res.send("Hello World!"));
+// General configurations
+dotenv.config();
 
+// App configuratiom
+const app: Application = express();
+
+app.use(compression());
+
+// Routes configuration
+app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
+
+// App running
 app.listen(PORT, () => {
+  // tslint:disable-next-line: no-console
   console.log(`Server started at http://localhost:${PORT}`);
 });
