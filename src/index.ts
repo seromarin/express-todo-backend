655ece8561c3066648a5473196c6fe921f9d00e6
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-05-17 16:00:49 -0500

    Basic api route & controller

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import express, { Request, Response, Application } from 'express';
+import express, { Request, Response, Application, Router } from 'express';
 import dotenv from 'dotenv';
 import compression from 'compression';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+
+// Controllers
+import { router as apiController } from './controllers/api.controller';
 
 // Global Variables
 const PORT = process.env.PORT || 3001;
@@ -12,9 +17,13 @@ dotenv.config();
 const app: Application = express();
 
 app.use(compression());
+app.use(helmet());
+app.use(bodyParser.json({ type: 'application/*+json' }))
 
 // Routes configuration
 app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
+
+app.use('/api', apiController);
 
 // App running
 app.listen(PORT, () => {
