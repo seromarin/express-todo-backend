43c79a8d29c7189faf2eb4dd1ab4429c82bb6cfd
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-05-17 15:18:15 -0500

    More configurations

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express, { Request, Response, Application } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: Application = express();
 
@@ -6,4 +9,6 @@ const PORT = process.env.PORT || 3001;
 
 app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
 
-app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`)
+});
