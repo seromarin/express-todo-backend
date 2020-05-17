0265f4a81f532ed5aa8fdd5214e7bb362824e311
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-05-17 15:28:05 -0500

    - Omit -

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app: Application = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
+app.get('/', (req: Request, res: Response) => res.send("Hello World!"));
 
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`)
