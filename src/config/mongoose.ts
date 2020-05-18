75287822f8656f119f475a12991c7b61f63f24b9
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-05-17 21:54:13 -0500

    .

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -4,6 +4,8 @@ const URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWOR
 
 mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connection.on('connected', () => console.log('Database connected :>> '))
+mongoose.connection.on('connected', () =>
+  console.log('Database connected :>> ')
+);
 
 export const mongooseConnection = mongoose.connection;
